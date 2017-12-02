1e51bcd6c3105e00e4657ed1c4f580193cb11a56
Robert Watson <spartan83@gmail.com>
2017-12-02 21:49:29 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,9 +1,12 @@
 def turn_count (board)
 cell = 0
-while cell !=9
-  
-turn+=1
-
+turn=0
+while cell !=8
+  if board[cell]== "X" || board[cell]== "O"
+    turn +=1
+  end
+  cell+=1
+end
 end
 
 def current_player (board)
